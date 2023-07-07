ea1bff10e2835daf95b1e905a4077c93ce412b43
LucasAudisio <82542357+LucasAudisio@users.noreply.github.com>
2023-07-07 11:27:34 -0300

    verify mail

--- accesosBases/accesoUsuario.ts
+++ accesosBases/accesoUsuario.ts
@@ -3,25 +3,34 @@ import { Collection, Db, Filter, FindCursor } from "mongodb";
 import { Usuario } from "../Clases/Usuario";
 import { createHash } from 'node:crypto';
 
-function sha256(content: string) {
+function sha256(content: string) {  
     return createHash('sha256').update(content).digest('hex')
 }
 
-export class AccesoUsuario {
+export class AccesoUsuario{
     url: String;
     database: Db;
     collection: Collection;
 
-    constructor(url: String, database: Db, collection: Collection) {
+    constructor(url: String, database: Db, collection: Collection){
         this.url = url;
         this.database = database;
         this.collection = collection;
     }
 
     public async getUsuario(dato: String) {
-        const filtro = { $or: [{ DNI: Number(dato) }, { mail: dato }] };
+        const filtro = { $or: [{DNI: Number(dato)}, {mail: dato}] };
         const usuario = await this.collection.findOne(filtro);
         return usuario;
+    }
+
+    subirDatos(usuario: Usuario){
+        usuario.contraseña = String(sha256(usuario.contraseña.toString()));
+        this.collection.insertOne(usuario);
+    }
+
+    public updateEstado(mail: String, estado: String){
+        this.collection.updateOne({mail:mail}, {$set: {estado: estado}});
     }
 
     public async login(mail: string, contraseña: string) {
@@ -48,11 +57,5 @@ export class AccesoUsuario {
             response.mensaje = "Usuario no encontrado"
             return response;
         }
-    }
-
-
-    subirDatos(usuario: Usuario) {
-        usuario.contraseña = String(sha256(usuario.contraseña.toString()));
-        this.collection.insertOne(usuario);
     }
 }
--- jwt.ts
+++ jwt.ts
@@ -1,19 +1,17 @@
 import jwt from "jsonwebtoken";
 
-const claveSecretaInv = "Utn-Clave-Inv";
-const claveSecretaAdmin = "Utn-Clave-Admin";
+const claveSecreta = "Secreto-Poli-Bet";
 
-//investigador
-export function generarClaveInv(nombre: String): string{
+export function generarClave(nombre: String): string{
     let dataFirma = {
         "nombre": nombre
     }
-    let respuesta = jwt.sign(dataFirma, claveSecretaInv);
+    let respuesta = jwt.sign(dataFirma, claveSecreta);
 
     return respuesta;
 }
 
-export function verificarClaveInv(req: any, res: any, next: any){
+export function verificarClave(req: any, res: any, next: any){
     const clave = req.headers.authorization;
 
     if (clave == undefined) {
@@ -21,40 +19,7 @@ export function verificarClaveInv(req: any, res: any, next: any){
     }
 
     try {
-        const payload: any = jwt.verify(clave, claveSecretaInv);
-        const nombreGenerado: string = payload.nombre;
-
-        const nombreSolicitud: string = req.body.nombreUsuario;
-
-        if (nombreGenerado !== nombreSolicitud) {
-            return res.status(401).send('Unauthorized: Invalid token.');
-        }
-        next();
-    }
-    catch (err) {
-        return res.status(401).send('Unauthorized: Invalid token.');
-    }
-}
-
-//Administrador
-export function generarClaveAdmin(nombre: String): string{
-    let dataFirma = {
-        "nombre": nombre
-    }
-    let respuesta = jwt.sign(dataFirma, claveSecretaAdmin);
-
-    return respuesta;
-}
-
-export function verificarClaveAdmin(req: any, res: any, next: any){
-    const clave = req.headers.authorization;
-
-    if (clave == undefined) {
-        return res.status(401).send('Unauthorized: No token provided.');
-    }
-
-    try {
-        const payload: any = jwt.verify(clave, claveSecretaAdmin);
+        const payload: any = jwt.verify(clave, claveSecreta);
         const nombreGenerado: string = payload.nombre;
 
         const nombreSolicitud: string = req.body.nombreUsuario;
